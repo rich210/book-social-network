ed53901c176cda86cc8805d81c2d129f238e4f96
rich210 <richespsan.210@gmail.com>
2024-06-15 12:18:21 -0600

    added environment variable to docker compose as well to build docker image

--- book-network-ui/src/app/app.config.server.ts
+++ book-network-ui/src/app/app.config.server.ts
@@ -1,10 +1,13 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { ApiModule } from './services/api.module';
+import { environment } from '../environments/environment';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    importProvidersFrom(ApiModule.forRoot({rootUrl:environment.apiUrl}))
   ]
 };
 
---
+++ book-network-ui/src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://bsn-api:8088/api/v1'  // Development API URL
+  };
+  
