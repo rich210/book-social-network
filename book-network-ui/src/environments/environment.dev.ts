f17b15695d6d5b35b93c8ad415195228c588ddea
rich210 <richespsan.210@gmail.com>
2024-06-17 17:16:38 -0600

    Updating frontend api url as well cors origin

--- book-network-ui/src/environments/environment.dev.ts
+++ book-network-ui/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://bsn-api:8088/api/v1'  // Development API URL
+    apiUrl: 'https://book-social-network-api.azurewebsites.net:8088/api/v1'  // Development API URL
   };
   
