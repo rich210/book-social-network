86eefb425a1bba1ee18797b603fbc76add701f34
rich210 <richespsan.210@gmail.com>
2024-06-17 16:48:18 -0600

    Adding root url

--- book-network-ui/src/app/app.config.ts
+++ book-network-ui/src/app/app.config.ts
@@ -1,14 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { httpTokenInterceptor } from './services/interceptor/http-token.interceptor';
+import { ApiModule } from './services/api.module';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(), 
-    provideHttpClient(withFetch(), withInterceptors([httpTokenInterceptor]))]
+    provideHttpClient(withFetch(), withInterceptors([httpTokenInterceptor])),
+    importProvidersFrom(ApiModule.forRoot({rootUrl:environment.apiUrl}))]
 };
