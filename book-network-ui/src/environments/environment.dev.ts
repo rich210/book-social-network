eac8c7cdce2940c4686373e6aacd3ce9317b05d3
rich210 <richespsan.210@gmail.com>
2024-06-18 15:43:22 -0600

    Fix backend url

--- book-network-ui/src/environments/environment.dev.ts
+++ book-network-ui/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://book-social-network-api.azurewebsites.net:8088/api/v1'  // Development API URL
+    apiUrl: 'https://book-social-network-api.azurewebsites.net/api/v1'  // Development API URL
   };
   
